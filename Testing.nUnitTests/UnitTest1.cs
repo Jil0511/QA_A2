f8f51bbc4f2e89b34e71c47db6c0decb27a39130
Jil0511 <jpatel5062@conestogac.on.ca>
2023-06-08 00:53:25 -0400

    Isosceles Traingle Tests

--- Testing.nUnitTests/UnitTest1.cs
+++ Testing.nUnitTests/UnitTest1.cs
@@ -21,16 +21,68 @@ namespace Testing.nUnitTests
                 int side2 = 35;
                 int side3 = 35;
 
-                string expected = "The triangle is valid and is an EQUILATERAL";
+                string output = "The triangle is valid and is an EQUILATERAL";
 
                 //Act
-                string actual = Triangle.AnalyzeTriangle(side1, side2, side3);
+                string original = Triangle.AnalyzeTriangle(side1, side2, side3);
 
                 //Assert
-                Assert.AreEqual(expected, actual);
+                Assert.AreEqual(output, original);
             
         }
 
+        [Test]
+        public void AnalyzeTriangle_Input40and50and50_OutputvalidISOSCELESTriangle()
+        {
+            //Arrange
+            int side1 = 40;
+            int side2 = 50;
+            int side3 = 50;
 
+            string output = "The triangle is valid and is an ISOSCELES";
+
+            //Act
+            string original = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            //Assert
+            Assert.AreEqual(output, original);
+
+        }
+
+        [Test]
+        public void AnalyzeTriangle_Input32and89and89_OutputvalidISOSCELESTriangle()
+        {
+            //Arrange
+            int side1 = 32;
+            int side2 = 89;
+            int side3 = 89;
+
+            string output = "The triangle is valid and is an ISOSCELES";
+
+            //Act
+            string original = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            //Assert
+            Assert.AreEqual(output, original);
+
+        }
+
+        [Test]
+        public void AnalyzeTriangle_Input24and73and73_OutputvalidISOSCELESTriangle()
+        {
+            //Arrange
+            int side1 = 24;
+            int side2 = 73;
+            int side3 = 73;
+
+            string output = "The triangle is valid and is an ISOSCELES";
+
+            //Act
+            string original = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            //Assert
+            Assert.AreEqual(output, original);
+
+        }
     }
 }
